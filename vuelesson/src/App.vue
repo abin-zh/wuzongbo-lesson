30f9e37c59078b9b5889a518f4f882bbf91942de
abin <863030357@qq.com>
2023-09-14 23:51:10 +0800

    create vue project

---
+++ vuelesson/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <RouterLink to="/">首页</RouterLink>
+    <RouterLink to="/about">关于</RouterLink>
+    <div>
+     <!-- 页面组件的占位 iframe -->
+     <RouterView></RouterView> 
+    </div>
+   </template>
+
+<style scoped>
+
+
+</style>
