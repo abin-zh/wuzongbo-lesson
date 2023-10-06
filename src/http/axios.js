6cc2327bae763a7889b17c4e9d4f98f70dcc6cab
abin <863030357@qq.com>
2023-10-06 18:19:47 +0800

    'test'

---
+++ src/http/axios.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    //请求地址(根)
+    baseURL:'https://api.jqrjq.cn/',
+    timeout: 6000
+});
+
+//拦截器
+
+instance.interceptors.request.use((config) => {
+    return config;
+},(err) => {
+    return Promise.reject(err);
+});
+
+instance.interceptors.response.use((res) => {
+    return res.data;
+},(err) => {
+    return Promise.reject(err);
+})
+
+export default instance;
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,29 @@
-<script setup>
-
+<script>
+    import { useCounterStore } from '@/stores/counter';
+    import { login } from '@/http'
+    export default{
+        setup(){
+            const mainStore = useCounterStore();
+            
+            return {
+                mainStore
+            }
+        },
+        methods:{
+            async testAxios(){
+                const testlogin = await login({
+                    "email": "414090297@qq.com",
+                    "password": "12345678"
+                });
+                console.log(testlogin);
+            }
+        }
+    }
 </script>
 
 <template>
-    <div>About</div>
+    <div @click="mainStore.increment()">{{ mainStore.count }}</div>
+    <el-button @click="testAxios"></el-button>
 </template>
 
 <style scoped>
