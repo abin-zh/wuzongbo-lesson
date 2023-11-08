c57fc59b1a40bef615766e121c08ae5b0139e073
abin <863030357@qq.com>
2023-11-08 16:42:21 +0800

    文章列表优化、详细文章

--- src/http/user.js
+++ src/http/user.js
@@ -1,5 +1,6 @@
 import axios from './axios'
 
+// 登录
 const userLogin = (data) => {
     return axios.request({
         url:'/api/mobile/elogin',
@@ -8,6 +9,7 @@ const userLogin = (data) => {
     })
 }
 
+// 注册
 const userReg = (data) => {
     return axios.request({
         url:'/api/mobile/eregister',
@@ -16,7 +18,17 @@ const userReg = (data) => {
     })
 }
 
+// 获取用户信息
+const getUserInfo = (id) => {
+    return axios.request({
+        url:`/api/userinfo/one?id=${id}`,
+        method:'get',
+    })
+
+}
+
 export {
     userLogin,
-    userReg
+    userReg,
+    getUserInfo
 }
